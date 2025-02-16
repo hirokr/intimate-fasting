6cf9a3344171bba3917b1f18a377ae0961c27d99
hirokr <hrr3645@gmail.com>
2025-02-16 21:40:57 +0600

    package update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
   experimental: {
-    ppr: true,
+    ppr: "incremental",
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
   },
 };
+
 
 export default nextConfig;
